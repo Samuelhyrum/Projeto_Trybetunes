75521050a976109ae33c5707ff46b17cc0b70f55
Samuel Hyrum <samuelhymp@gmail.com>
2022-08-01 16:24:47 -0300

    Requisito 1

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div data-testid="page-login" value="page-login">
+        Login
+      </div>
+
+    );
+  }
+}
+
+export default Login;
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile" value="page-profile">
+        Profile
+      </div>
+    );
+  }
+}
+
+export default Profile;
