d005a4d908f7479db53d4ed7473940a5f488d767
Samuel Hyrum <samuelhymp@gmail.com>
2022-08-02 19:08:51 -0300

    Requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends Component {
   state = {
     buttonDisable: true,
     band: '',
+    artists: '',
+    albums: [],
+    loading: false,
   };
 
   handleChangeBand = ({ target }) => {
@@ -13,6 +19,7 @@ class Search extends Component {
       [name]: value,
     }, () => {
       const { band } = this.state;
+      console.log(band);
       const minLength = 2;
       if (band.length >= minLength) {
         this.setState({ buttonDisable: false });
@@ -20,36 +27,74 @@ class Search extends Component {
     });
   };
 
-  render() {
-    const { buttonDisable, band } = this.state;
-    return (
-      <main>
-        <Header />
-        <div data-testid="page-search" value="page-search">
-          <form>
-            <input
-              data-testid="search-artist-input"
-              type="text"
-              name="band"
-              value={ band }
-              placeholder="Digite o nome da banda"
-              onChange={ this.handleChangeBand }
-            />
-            <br />
-            <button
-              data-testid="search-artist-button"
-              name="Pesquisar"
-              value="Pesquisar"
-              type="button"
-              disabled={ buttonDisable }
-            >
-              Pesquisar
-            </button>
-          </form>
-        </div>
-      </main>
-    );
-  }
+ searchBand = async () => {
+   const { band } = this.state;
+   this.setState({
+     artists: band,
+     loading: true,
+     albums: await searchAlbumsAPI(band) }, () => {
+     this.setState({
+       loading: false,
+       band: '',
+     });
+   });
+ }
+
+ render() {
+   const { buttonDisable, band, albums, loading, artists } = this.state;
+   let vazia = '';
+   if (albums.length > 0) {
+     vazia = (
+       <p>
+         {`Resultado de álbuns de: ${artists}`}
+       </p>);
+   } else if (albums.length === 0) {
+     vazia = (
+       <p>Nenhum álbum foi encontrado</p>
+     );
+   }
+   return (
+     <div data-testid="page-search" value="page-search">
+       <Header />
+       <form>
+         <div>
+           <input
+             data-testid="search-artist-input"
+             type="text"
+             name="band"
+             value={ band }
+             placeholder="Digite o nome da banda"
+             onChange={ this.handleChangeBand }
+           />
+         </div>
+         <button
+           data-testid="search-artist-button"
+           name="Pesquisar"
+           value="Pesquisar"
+           type="button"
+           disabled={ buttonDisable }
+           onClick={ this.searchBand }
+         >
+           Pesquisar
+         </button>
+       </form>
+       {loading ? <Loading /> : vazia }
+       {albums.map((album) => (
+         <div
+           key={ album.collectionId }
+         >
+           <img src={ album.artworkUrl100 } alt="ARTISTA" />
+           <p>{album.collectionName}</p>
+           <p>{album.artistName}</p>
+           <Link
+             to={ `/album/${album.collectionId}` }
+             data-testid={ `link-to-album-${album.collectionId}` }
+           />
+         </div>
+       ))}
+     </div>
+   );
+ }
 }
 
 export default Search;
